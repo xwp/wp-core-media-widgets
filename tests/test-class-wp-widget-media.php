b552f5243429c40c98ca712a78f387d3feac6177
Weston Ruter <weston@xwp.co>
2017-03-21 15:33:24 -0700

    Use phpunit class mock instead of creating a test subclass

--- tests/test-class-wp-widget-media.php
+++ tests/test-class-wp-widget-media.php
@@ -1,12 +1,36 @@
 <?php
 
-class Test_WP_Media_Widget extends WP_UnitTestCase {
+class Test_WP_Widget_Media extends WP_UnitTestCase {
 
 	/**
-	 * @covers WP_Widget_Media::update
+	 * Get instance for mocked media widget class.
+	 *
+	 * @return PHPUnit_Framework_MockObject_MockObject|WP_Widget_Media Mocked instance.
+	 */
+	function get_mocked_class_instance() {
+		$original_class_name = 'WP_Widget_Media';
+		$arguments = array(
+			'mocked',
+			'Mocked',
+		);
+		$mock_class_name = 'WP_Widget_Media_Mocked';
+		$call_original_constructor = true;
+		$call_original_clone = true;
+		$call_autoload = true;
+		$mocked_methods = array( 'render_media' );
+
+		$widget = $this->getMockForAbstractClass( $original_class_name, $arguments, $mock_class_name, $call_original_constructor, $call_original_clone, $call_autoload, $mocked_methods );
+
+		return $widget;
+	}
+
+	/**
+	 * @covers WP_Widget_Media::update()
 	 */
 	function test_update() {
-		$widget = new Test_Media_Widget( 'media', 'Media' );
+
+		$widget = $this->get_mocked_class_instance();
+
 		$instance = array();
 
 		// Should return valid attachment ID.
@@ -39,38 +63,43 @@ class Test_WP_Media_Widget extends WP_UnitTestCase {
 		// Should filter invalid key.
 		$result = $widget->update( array( 'imaginary_key' => 'value' ), $instance );
 		$this->assertSame( $result, $instance );
+
+		$widget->render_media( array(), array() );
 	}
 
 	/**
-	 * @covers WP_Widget_Media::widget
+	 * @covers WP_Widget_Media::widget()
 	 */
 	function test_widget() {
-		$widget = new Test_Media_Widget( 'media', 'Media' );
-
 		$args = array(
 			'before_title'  => '<h2>',
 			'after_title'   => "</h2>\n",
 			'before_widget' => '<section>',
 			'after_widget'  => "</section>\n",
 		);
-		$instance = array( 'title' => 'Buscar' );
+		$instance = array(
+			'title' => 'Foo',
+			'url' => '',
+			'attachment_id' => 0,
+		);
 
 		ob_start();
+		$widget = $this->get_mocked_class_instance();
+		$widget->expects( $this->atLeastOnce() )->method( 'render_media' )->with( $instance );
 		$widget->widget( $args, $instance );
 		$output = ob_get_clean();
 
-		$this->assertContains( '<h2>Buscar</h2>', $output );
+		$this->assertContains( '<h2>Foo</h2>', $output );
 		$this->assertContains( '<section>', $output );
 		$this->assertContains( '</section>', $output );
 
-		// No title
+		// No title.
 		ob_start();
+		$widget = $this->get_mocked_class_instance();
+		$instance['title'] = '';
+		$widget->expects( $this->atLeastOnce() )->method( 'render_media' )->with( $instance );
 		$widget->widget( $args, array() );
 		$output = ob_get_clean();
-		$this->assertNotContains( '<h2>Buscar</h2>', $output );
+		$this->assertNotContains( '<h2>Foo</h2>', $output );
 	}
-}
-
-class Test_Media_Widget extends WP_Widget_Media {
-	public function render_media( $instance ) {}
 }
