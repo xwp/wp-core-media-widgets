36fc72588a56e3339d83d061ac2d6dee1afe8fb5
Adam Silverstein <adam@10up.com>
2017-04-05 09:40:09 -0400

    Simplify the widget image preview - only use image url

--- wp-includes/widgets/class-wp-widget-image.php
+++ wp-includes/widgets/class-wp-widget-image.php
@@ -291,12 +291,8 @@ class WP_Widget_Image extends WP_Widget_Media {
 				<div class="notice notice-error notice-alt">
 					<p><?php _e( 'Unable to preview media due to an unknown error.' ); ?></p>
 				</div>
-			<# } else if ( 'image' === data.attachment.type && data.attachment.sizes && data.attachment.sizes.medium ) { #>
-				<img class="attachment-thumb" src="{{ data.attachment.sizes.medium.url }}" draggable="false" alt="" />
-			<# } else if ( 'image' === data.attachment.type && data.attachment.sizes && data.attachment.sizes.full ) { #>
-				<img class="attachment-thumb" src="{{ data.attachment.sizes.full.url }}" draggable="false" alt="" />
-			<# } else if ( 0 === data.attachment.attachment_id && '' !== data.attachment.url ) { #>
-				<img class="attachment-thumb remote-image" src="{{ data.attachment.url }}" draggable="false" alt="" />
+			<# } else if ( data.attachment.url ) { #>
+				<img class="attachment-thumb" src="{{ data.attachment.url }}" draggable="false" alt="" />
 			<# } #>
 		</script>
 		<?php
