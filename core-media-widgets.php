dcb17ffac72b259144ec5c81de451987f07cd283
Weston Ruter <weston@xwp.co>
2017-04-23 16:54:54 -0400

    Add video and audio scripts to qunit suite

--- core-media-widgets.php
+++ core-media-widgets.php
@@ -42,7 +42,6 @@ function wp32417_default_scripts( WP_Scripts $scripts ) {
 
 	$scripts->add( 'media-audio-widget', plugin_dir_url( __FILE__ ) . 'wp-admin/js/widgets/media-audio-widget.js', array( 'media-widgets' ) );
 	$scripts->add( 'media-image-widget', plugin_dir_url( __FILE__ ) . 'wp-admin/js/widgets/media-image-widget.js', array( 'media-widgets' ) );
-
 	$scripts->add( 'media-video-widget', plugin_dir_url( __FILE__ ) . 'wp-admin/js/widgets/media-video-widget.js', array( 'media-widgets' ) );
 
 	$scripts->add_inline_script( 'customize-selective-refresh', file_get_contents( dirname( __FILE__ ) . '/wp-includes/js/customize-selective-refresh-extras.js' ) );
