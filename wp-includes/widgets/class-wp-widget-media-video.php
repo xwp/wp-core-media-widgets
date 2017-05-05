f2f4d70494181fdecadb1f2b9af8cf2dafca44b9
Konstantin Obenland <obenland@automattic.com>
2017-05-05 10:24:23 -0700

    Whitelist should_preview_update for video/audio (#135)
    Prevents the preview from updating when certain props change.

--- wp-includes/widgets/class-wp-widget-media-video.php
+++ wp-includes/widgets/class-wp-widget-media-video.php
@@ -187,7 +187,7 @@ class WP_Widget_Media_Video extends WP_Widget_Media {
 
 		$exported_schema = array();
 		foreach ( $this->get_instance_schema() as $field => $field_schema ) {
-			$exported_schema[ $field ] = wp_array_slice_assoc( $field_schema, array( 'type', 'default', 'enum', 'minimum', 'format', 'media_prop' ) );
+			$exported_schema[ $field ] = wp_array_slice_assoc( $field_schema, array( 'type', 'default', 'enum', 'minimum', 'format', 'media_prop', 'should_preview_update' ) );
 		}
 		wp_add_inline_script(
 			$handle,
