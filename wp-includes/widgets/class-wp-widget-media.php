34502bd6c5ce7ca49cb8319d3a88708fa1ddaf26
Weston Ruter <weston@xwp.co>
2017-05-09 16:09:28 -0700

    Prevent doubled enqueueing and printing of templates in customizer

--- wp-includes/widgets/class-wp-widget-media.php
+++ wp-includes/widgets/class-wp-widget-media.php
@@ -88,14 +88,15 @@ abstract class WP_Widget_Media extends WP_Widget {
 	 */
 	public function _register() {
 
+		// Note that the widgets component in the customizer will also do the 'admin_print_scripts-widgets.php' action in WP_Customize_Widgets::print_scripts().
 		add_action( 'admin_print_scripts-widgets.php', array( $this, 'enqueue_admin_scripts' ) );
-		add_action( 'customize_controls_print_scripts', array( $this, 'enqueue_admin_scripts' ) );
+
 		if ( $this->is_preview() ) {
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_preview_scripts' ) );
 		}
 
+		// Note that the widgets component in the customizer will also do the 'admin_footer-widgets.php' action in WP_Customize_Widgets::print_footer_scripts().
 		add_action( 'admin_footer-widgets.php', array( $this, 'render_control_template_scripts' ) );
-		add_action( 'customize_controls_print_footer_scripts', array( $this, 'render_control_template_scripts' ) );
 
 		add_filter( 'display_media_states', array( $this, 'display_media_state' ), 10, 2 );
 
