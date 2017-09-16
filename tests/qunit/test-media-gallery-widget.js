f2c6a49ccdff4837b15113afe21e69b24321307a
Weston Ruter <weston@xwp.co>
2017-09-15 21:36:43 -0700

    Remove obsolete qunit test

--- tests/qunit/test-media-gallery-widget.js
+++ tests/qunit/test-media-gallery-widget.js
@@ -19,28 +19,15 @@
 	// TODO PREVIEW TESTS.
 
 	test( 'gallery media model', function() {
-		var GalleryWidgetModel, galleryWidgetModelInstance, attachmentData;
+		var GalleryWidgetModel, galleryWidgetModelInstance;
 		equal( typeof wp.mediaWidgets.modelConstructors.media_gallery, 'function', 'wp.mediaWidgets.modelConstructors.media_gallery is a function' );
 		GalleryWidgetModel = wp.mediaWidgets.modelConstructors.media_gallery;
 		ok( GalleryWidgetModel.prototype instanceof wp.mediaWidgets.MediaWidgetModel, 'wp.mediaWidgets.modelConstructors.media_gallery subclasses wp.mediaWidgets.MediaWidgetModel' );
 
-		attachmentData = '[{"id":42,"url":"http://src.wordpress-develop.dev/wp-content/uploads/2017/04/img_20170407_080914.jpg"},{"id":49,"url":"http://src.wordpress-develop.dev/wp-content/uploads/2017/04/img_20170407_080915.jpg"}]';
 		galleryWidgetModelInstance = new GalleryWidgetModel();
 		_.each( galleryWidgetModelInstance.attributes, function( value, key ) {
 			equal( value, GalleryWidgetModel.prototype.schema[ key ][ 'default' ], 'Should properly set default for ' + key );
 		});
-
-		// Test removeAttachmentId().
-		galleryWidgetModelInstance.set( {
-			'attachments': attachmentData,
-			'ids': '42,49'
-		} );
-		galleryWidgetModelInstance.removeAttachmentId();
-		equal( galleryWidgetModelInstance.get( 'attachments' ), attachmentData );
-		equal( galleryWidgetModelInstance.get( 'ids' ), '42,49' );
-		galleryWidgetModelInstance.removeAttachmentId( 49 );
-		equal( galleryWidgetModelInstance.get( 'attachments' ), '[{"id":42,"url":"http://src.wordpress-develop.dev/wp-content/uploads/2017/04/img_20170407_080914.jpg"}]' );
-		equal( galleryWidgetModelInstance.get( 'ids' ), '42' );
 	});
 
 })();
